f95224f113629b1d95287ebc605abb5e7f3f20a0
Felps06 <https://github.com/Felps06/dados.git>
2023-08-11 22:01:29 -0300

    atv1

---
+++ atv01.py
@@ -0,0 +1,20 @@
+# nome, sobrenome, idade, altura, peso, maior_de_idade #
+
+nome = "Felipe"
+
+sobrenome = "Gabriel"
+
+idade = 17
+
+altura = 1.84
+
+peso = 105
+
+maior_de_idade = idade >= 18
+
+print("nome", nome)
+print("sobrenome", sobrenome)
+print("idade", idade, "anos")
+print("altura", altura, "metros")
+print("peso", peso, "kg")
+print("maior_de_idade", "sim" if maior_de_idade else "não")
