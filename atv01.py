b601cd04b5616a2f8a89b828019738df614dcc84
Felps06 <141460553+Felps06@users.noreply.github.com>
2023-08-15 11:05:43 -0300

    Update atv01.py

--- atv01.py
+++ atv01.py
@@ -19,29 +19,3 @@ print("altura", altura, "metros")
 print("peso", peso, "kg")
 print("maior_de_idade", "sim" if maior_de_idade else "não")
 
-
-sair = "sair"
-
-numero01 = input("digite o primeiro numero")
-numero02 = input("digite o segundo numero")
-operador = input("digite o operador (+-*/)")
- 
-
-numero01_float = 0
-numero02_float = 0
-
-numero01_float = float(numero01)
-numero02_float = float(numero02)
-
-if operador == "+":
- print(f"{numero01_float} + {numero02_float} = " , numero02_float + numero01_float)
-
-elif operador == "-":
- print(f"{numero01_float} - {numero02_float} = " , numero02_float - numero01_float)
-
-elif operador == "*":
- print(f"{numero01_float} * {numero02_float} = " , numero02_float * numero01_float)
-
-elif operador == "/":
- print(f"{numero01_float} / {numero02_float} = " , numero02_float / numero01_float)
-
